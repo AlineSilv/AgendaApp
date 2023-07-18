feaf39e824875c9d7f5d2258be5429f801444da4
Aline Silva <alinealv.silv@gmail.com>
2023-07-17 22:16:05 -0300

    commit

---
+++ agenda-telefonica/apps/aplication-agenda/frontend/src/components/Form.js
@@ -0,0 +1,116 @@
+import axios from "axios";
+import React, { useEffect, useRef } from "react";
+import styled from "styled-components";
+import { toast } from "react-toastify";
+
+const FormContainer = styled.form`
+  display: flex;
+  align-items: flex-end;
+  gap: 10px;
+  flex-wrap: wrap;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 5px;
+`;
+
+const InputArea = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  width: 120px;
+  padding: 0 10px;
+  border: 1px solid #bbb;
+  border-radius: 5px;
+  height: 40px;
+`;
+
+const Label = styled.label``;
+
+const Button = styled.button`
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+  background-color: #808080;
+  color: white;
+  height: 42px;
+`;
+
+const Form = ({ getUsers, onEdit, setOnEdit }) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    if (onEdit) {
+      const user = ref.current;
+
+      user.nome.value = onEdit.nome;
+      user.idade.value = onEdit.idade;
+      user.fone.value = onEdit.fone;
+    }
+  }, [onEdit]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const user = ref.current;
+
+    if (
+      !user.nome.value ||
+      !user.idade.value ||
+      !user.fone.value 
+    ) {
+      return toast.warn("Preencha todos os campos!");
+    }
+
+    if (onEdit) {
+      await axios
+        .put("http://localhost:8800/" + onEdit.id, {
+          nome: user.nome.value,
+          idade: user.idade.value,
+          fone: user.fone.value,
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    } else {
+      await axios
+        .post("http://localhost:8800", {
+          nome: user.nome.value,
+          idade: user.idade.value,
+          fone: user.fone.value,
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    }
+
+    user.nome.value = "";
+    user.idade.value = "";
+    user.fone.value = "";
+
+    setOnEdit(null);
+    getUsers();
+  };
+
+  return (
+    <FormContainer ref={ref} onSubmit={handleSubmit}>
+      <InputArea>
+        <Label>Nome</Label>
+        <Input name="nome" />
+      </InputArea>
+      <InputArea>
+        <Label>Idade</Label>
+        <Input name="idade" />
+      </InputArea>
+      <InputArea>
+        <Label>Telefone</Label>
+        <Input name="fone" />
+      </InputArea>
+
+      <Button type="submit">REGISTRAR</Button>
+    </FormContainer>
+  );
+};
+
+export default Form;
